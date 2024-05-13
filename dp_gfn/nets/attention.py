dd28246a132b07e4fd280bbbf4ca78d3275d63e5
nv259 <nguyenvietnhat.03@gmail.com>
2024-05-13 17:40:01 +0700

    update project base structure

---
+++ dp_gfn/nets/attention.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+
+class LinearMultiHeadAttention(nn.Module):
+    def __init__(self, d_model, n_heads):
+        super(LinearMultiHeadAttention, self).__init__()
+    
+    def forward(self, x):
+        pass
+
+    
