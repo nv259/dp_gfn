b940efb9ccb62c9d07a6dff1aa5209cde4518722
nv259 <nguyenvietnhat.03@gmail.com>
2024-05-29 17:20:59 +0700

    initialize Batched State Graph

--- dp_gfn/gfn.py
+++ dp_gfn/gfn.py
@@ -1,5 +1,5 @@
 import torch
-from dp_gfn.utils.masking import encode, decode, base_mask
+from dp_gfn.utils.masking import base_mask, decode, encode
 
 
 def get_reward():
@@ -23,29 +23,42 @@ def setup():
 
 
 class StateBatch:
-    
     def __init__(self, edges, labels, num_variables, device, root_first=True):
         self.num_variables = num_variables
         self.device = device
-        
+
         self._data = {
             "edges": edges.to(device),
             "labels": labels.to(device),
-            "mask": encode(base_mask(num_variables=num_variables, root_first=root_first, device=device).repeat(edges.shape[0], 1, 1)),
-            "adjacency": encode(torch.zeros(edges.shape[0], num_variables, num_variables, device=device)),
-            "isolated_root": torch.ones(edges.shape[0], dtype=torch.bool, device=device),
+            "mask": encode(
+                base_mask(
+                    num_variables=num_variables, root_first=root_first, device=device
+                ).repeat(edges.shape[0], 1, 1)
+            ),
+            "adjacency": encode(
+                torch.zeros(edges.shape[0], num_variables, num_variables, device=device)
+            ),
+            "isolated_root": torch.ones(
+                edges.shape[0], dtype=torch.bool, device=device
+            ),
             # "num_edges": torch.zeros(edges.shape[0], dtype=torch.int).to(device),
             "num_variables": num_variables.to(device),
-            "done": torch.zeros(edges.shape[0], dtype=torch.bool, device=device)
-        }   
+            "done": torch.zeros(edges.shape[0], dtype=torch.bool, device=device),
+        }
         self._closure_T = torch.eye(num_variables, dtype=torch.bool, device=device)
         self._closure_T = self._closure_T.repeat(edges.shape[0], 1, 1)
-    
-    def __getitem__(self, ):
+
+    def __getitem__(
+        self,
+    ):
         pass
-    
-    def step(self, ):
+
+    def step(
+        self,
+    ):
         pass
-    
-    def reset(self, ):
+
+    def reset(
+        self,
+    ):
         pass
