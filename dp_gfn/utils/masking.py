b588a50e22752662dc299f712ad2f717f2a077ed
nv259 <nguyenvietnhat.03@gmail.com>
2024-05-29 17:35:23 +0700

    decode() fn for unpacking bits from the encoded

--- dp_gfn/utils/masking.py
+++ dp_gfn/utils/masking.py
@@ -7,8 +7,12 @@ def encode(decoded):
     return torch.from_numpy(np.packbits(encoded, axis=1)) 
 
 
-def decode(encoded, dtype=np.float32):
-    pass
+def decode(encoded, num_variables, dtype=torch.float32):
+    encoded = encoded.detach().cpu().numpy()
+    decoded = np.unpackbits(encoded, axis=-1, count=num_variables ** 2) 
+    decoded = decoded.reshape(*encoded.shape[:-1], num_variables, num_variables)
+    
+    return torch.from_numpy(decoded).to(dtype)
 
 
 def base_mask(num_variables, root_first=True, device=torch.device('cpu')) -> torch.Tensor:
