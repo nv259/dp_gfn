2ad9d7dbfd289f00d3bd88a4c27a539a1d83fecc
nv259 <nguyenvietnhat.03@gmail.com>
2024-05-29 17:25:50 +0700

    base utils for masking over actions

---
+++ dp_gfn/utils/masking.py
@@ -0,0 +1,14 @@
+import numpy as np
+import torch
+
+
+def encode(decoded):
+    pass
+
+
+def decode(encoded, dtype=np.float32):
+    pass
+
+
+def base_mask(num_variables, root_first=True, device=torch.device('cpu')) -> torch.Tensor:
+    pass
