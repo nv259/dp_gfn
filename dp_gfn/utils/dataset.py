7ff3486757ba67f23d345d550e6dbbbc22d5bc2b
nv259 <nguyenvietnhat.03@gmail.com>
2024-06-08 17:29:54 +0700

    Base CONLLU-format dataset

--- dp_gfn/utils/dataset.py
+++ dp_gfn/utils/dataset.py
@@ -1,8 +1,11 @@
-import torch
+import os
+import xml.etree.ElementTree as ET
+
 import conllu
 import networkx as nx
+import numpy as np
+import torch
 from torch.utils.data import DataLoader, Dataset
-import xml.etree.ElementTree as ET
 
 
 def parse_conllu_for_edges(current_node: conllu.models.TokenTree, parent_index: int):
@@ -20,36 +23,45 @@ def parse_conllu_for_edges(current_node: conllu.models.TokenTree, parent_index: 
     return edges
 
 
-def adjacency_matrix_from_edges_list(edges_list, num_variables: int) -> torch.Tensor: 
-    G = torch.zeros(num_variables, num_variables, dtype=torch.int)
-    for source, target, tag in edges_list: 
+def adjacency_matrix_from_edges_list(edges_list, num_variables: int) -> torch.Tensor:
+    G = torch.zeros(num_variables + 1, num_variables + 1, dtype=torch.int)
+    for source, target, tag in edges_list:
         G[source, target] = tag
-     
+
     return G
 
 
 def get_dependency_relation_dict(path_to_stats_file: str) -> dict:
     tree = ET.parse(path_to_stats_file)
     root = tree.getroot()
-    deps = root.find('deps')
-    
+    deps = root.find("deps")
+
     rel_id = {}
-    for id, dep in enumerate(deps.findall('dep')):
-        rel = dep.get('name')
-        rel_id[rel] = id
-        
-    return rel_id 
+    for idx, dep in enumerate(deps.findall("dep")):
+        rel = dep.get("name")
+        rel_id[rel] = idx
+
+    return rel_id
 
 
 class BaseDataset(Dataset):
-    def __init__(self, path_to_conllu_file: str, max_num_nodes: int = 160, store_nx_graph: bool = False):
+    def __init__(
+        self,
+        path_to_conllu_file: str,
+        max_num_nodes: int = 160,
+        store_nx_graph: bool = False,
+    ):
         super(BaseDataset, self).__init__()
 
         self.path = path_to_conllu_file
         self.store_nx_graph = store_nx_graph
         self.max_num_nodes = max_num_nodes
-        self.data = []
+        self.rel_id = get_dependency_relation_dict(
+            os.path.join(os.path.dirname(path_to_conllu_file), "stats.xml")
+        )
+        self.id_rel = {v: k for k, v in self.rel_id.items()}
 
+        self.data = []
         with open(self.path, "r", encoding="utf-8") as data_file:
             for tokenlist in conllu.parse_tree_incr(data_file):
                 self.data.append(tokenlist)
@@ -59,12 +71,15 @@ class BaseDataset(Dataset):
 
     def __getitem__(self, index):
         item = self.data[index]
-        
+
         edges_list = parse_conllu_for_edges(item, parent_index=0)
-        
+        edges_list = [(source, target, self.rel_id[tag]) for source, target, tag in edges_list]
+
         if self.store_nx_graph:
             G = nx.from_edgelist(edges_list)
         else:
-            G = adjacency_matrix_from_edges_list(edges_list, num_variables=self.max_num_nodes)
-            
+            G = adjacency_matrix_from_edges_list(
+                edges_list, num_variables=self.max_num_nodes
+            )
+
         return {"metadata": item.metadata, "graph": G}
