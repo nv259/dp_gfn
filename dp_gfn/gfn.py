e6f67b24a047a74436ef1f9f0b7364c390b99f3b
nv259 <nguyenvietnhat.03@gmail.com>
2024-06-19 14:30:42 +0700

    tmp

--- dp_gfn/gfn.py
+++ dp_gfn/gfn.py
@@ -117,6 +117,7 @@ class DPGFN:
         traj_log_prob = torch.zeros(self.batch_size, device=self.device)
 
         for t in range(self.max_number_of_words):
+            print(t)
             logits = self.model(batch["edges"], batch["labels"])
             logits = masking.mask_logits(logits, batch["mask"])
             uniform_logits = masking.mask_uniform_logits(logits, batch["mask"]).to(
