3d401d632c2351ca4a7a942f4c9b0ec5e75a195e
nv259 <nguyenvietnhat.03@gmail.com>
2024-07-02 14:53:44 +0700

    create Embeddings class

--- dp_gfn/nets/bert.py
+++ dp_gfn/nets/bert.py
@@ -2,13 +2,16 @@ import numpy as np
 import jax.numpy as jnp
 import jax 
 import haiku as hk
-from transformers import AutoModel, AutoConfig, AutoTokenizer
+from transformers import BertModel, BertConfig, BertTokenizer
 
 
 class PretrainedWeights(object):
     
-    def __init__(self, weights):
-        self.weights = weights
+    def __init__(self, path_to_pretrained):
+        self.path_to_pretrained = path_to_pretrained
+        
+        pretrained_model = BertModel.from_pretrained(path_to_pretrained)
+        self.weights = dict(pretrained_model.named_parameters())
         
     def __getitem__(self, path_to_weight):
         if isinstance(path_to_weight, list):
@@ -23,4 +26,43 @@ class PretrainedWeights(object):
    
     def __str__(self) -> str:
         return str(self.weights.keys())
+
+
+class Embeddings(hk.Module):
+    
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        
+    def __call__(self, training=False, **kwargs):
+        input_ids = kwargs['input_ids']
+        token_type_ids = kwargs['token_type_ids']
+        attention_mask = kwargs['attention_mask']
+        
+        # Calculate embeddings
+        token_embeddings = WordEmbeddings(self.config)(input_ids)
+        position_embeddings = PositionEmbeddings(self.config)()
+        token_type_embeddings = TokenTypeEmbeddings(self.config)(token_type_ids)
+        
+        embeddings = token_embeddings + position_embeddings + token_type_embeddings
+        
+        # LayerNorm
+        embeddings = hk.LayerNorm(
+            axis=-1, 
+            create_offset=True, 
+            create_scale=True, 
+            eps=self.config['layer_norm_eps'],
+            scale_init=hk.initializers.Constant(
+                pretrained_weights['embeddings.LayerNorm.weight']),
+            offset_init=hk.initializers.Constant(
+                pretrained_weights['embeddings.LayerNorm.bias']),
+            name='LayerNorm'
+        )(embeddings)
+        
+        if training:
+            embeddings = hk.dropout(
+                hk.next_rng_key(), rate=self.config['hidden_dropout_prob']
+            )
+        
+        return embeddings
     
