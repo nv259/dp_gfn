a8d933f65bb6ae77f8778a7de3a8efd31749a56d
nv259 <nguyenvietnhat.03@gmail.com>
2024-07-26 16:33:52 +0700

    remove hydra

--- train.py
+++ train.py
@@ -13,32 +13,12 @@ from dp_gfn.utils.misc import flatten_config
 from dp_gfn.utils.data import get_dataloader
 
 
-def set_seed(seed=42):
-    torch.backends.cudnn.benchmark = False
-    torch.backends.cudnn.deterministic = True
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-
-
-def check_model_num_tags(model, orig_num_tags):
-    assert (
-        model.num_tags
-        == model.state_encoder.num_tags
-        == model.backbone.layers[0].num_tags
-        == model.label_scorer.num_tags
-        == orig_num_tags + 2
-    )
-
-
 @hydra.main(config_path="./configs", config_name="main")
 def main(config):
     os.chdir(hydra.utils.get_original_cwd())
     config.seed = random.randint(1, 100000) if config.seed is None else config.seed
     config.model.num_variables = config.max_number_of_words + 1 
     config.max_number_of_words += 1 # ROOT token
-    # config.model.backbone.encoder_block.label_embedded = not config.model.state_encoder.encode_label
  
     log_config = flatten_config(OmegaConf.to_container(config, resolve=True), sep="/")
     log_config = {"/".join(("config", key)): val for key, val in log_config.items()}
@@ -74,11 +54,9 @@ def main(config):
         logging.warning("No validation data found")
     
     logging.info("Initializing Model")
-    model = hydra.utils.instantiate(config.model, num_tags=num_tags)
-    check_model_num_tags(model, num_tags)
     
     logging.info("Initializing Algorithm")
-    algorithm = DPGFN(config=config, model=model)
+    algorithm = DPGFN(config=config, num_tags=num_tags)
     
     algorithm.train(train_loader=train_loader, val_loader=val_loader)
 
