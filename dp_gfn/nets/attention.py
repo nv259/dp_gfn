bc587b52be86a5c7748b849a8ac74f7fc9147400
nv259 <nguyenvietnhat.03@gmail.com>
2024-08-13 18:18:42 +0700

    incorporate edges' label information into attention mechanism

--- dp_gfn/nets/attention.py
+++ dp_gfn/nets/attention.py
@@ -1,26 +1,4 @@
-"""
-MIT License
-
-Copyright (c) 2022 Tristan Deleu
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
+## the base code is from https://github.com/tristandeleu/jax-dag-gflownet
 import jax.numpy as jnp
 import haiku as hk
 
@@ -28,7 +6,7 @@ import jax.nn as nn
 
 
 class LinearMultiHeadAttention(hk.MultiHeadAttention):
-    def __call__(self, query, key, value, mask=None):
+    def __call__(self, query, key, value, arc, mask=None):
         feature_map = lambda x: nn.elu(x) + 1.0
         eps = 1e-6
 
@@ -36,6 +14,11 @@ class LinearMultiHeadAttention(hk.MultiHeadAttention):
         key_heads = self._linear_projection(key, self.key_size, "key")
         value_heads = self._linear_projection(value, self.value_size, "value")
 
+        arc_keys = hk.Embed(self.num_tags, self.key_size, name="relation2keys")(arc)
+        arc_values = hk.Embed(self.num_tags, self.key_size, name="relation2values")(arc)
+        key_heads = key_heads + arc_keys
+        value_heads = value_heads + arc_values
+        
         # Map the query & key with a feature map
         query_heads = feature_map(query_heads)
         key_heads = feature_map(key_heads)
