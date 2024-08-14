0e6383caef8df71fc96cf74b12d416f535d3f6d1
nv259 <nguyenvietnhat.03@gmail.com>
2024-08-14 10:35:57 +0700

    change name of label_scorer for more consistent code

--- dp_gfn/nets/initial_encoders.py
+++ dp_gfn/nets/initial_encoders.py
@@ -68,7 +68,7 @@ class PrefEncoder(hk.Module):
         return word_embeddings
 
 
-class LabelScorer(hk.Module):
+class Biaffine(hk.Module):
     def __init__(self, num_tags, intermediate_dim=128):
         super().__init__()
 
@@ -105,6 +105,6 @@ class LabelScorer(hk.Module):
 
 
 def label_score_fn(head, dep, num_tags):
-    lab_score = LabelScorer(num_tags=num_tags)(head, dep)
+    lab_score = Biaffine(num_tags=num_tags)(head, dep)
 
     return lab_score 
