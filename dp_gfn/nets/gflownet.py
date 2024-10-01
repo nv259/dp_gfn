8a20d89dcf27614eb199fd5a904fb8ac13bcd854
nv259 <nguyenvietnhat.03@gmail.com>
2024-10-01 03:52:46 +0000

    revise dpgfn model's call

--- dp_gfn/nets/gflownet.py
+++ dp_gfn/nets/gflownet.py
@@ -9,7 +9,7 @@ from dp_gfn.utils import masking
 class DPGFlowNet(hk.Module):
     def __init__(
         self,
-        num_variables=160,
+        num_variables=100,
         num_tags=41,
         num_layers=5,
         num_heads=4,
@@ -30,7 +30,7 @@ class DPGFlowNet(hk.Module):
 
         self.init_scale = 2.0 / self.num_layers
 
-    def __call__(self, key, x, node_id, labels, masks, delta):
+    def __call__(self, key, x, labels, mask, delta):
         for _ in range(self.num_layers):
             x = LinearTransformerBlock(
                 num_heads=self.num_heads,
@@ -40,12 +40,11 @@ class DPGFlowNet(hk.Module):
                 num_tags=self.num_tags,
             )(x, labels)
             
-        # TODO: Should we use only visitted nodes to predict next node to visit?
-        log_pi_dep = self.next_node_policy(x.mean(axis=-2), x, masks[1])  
-        key, dep_id, log_pF_dep = masking.sample_action(key, log_pi_dep, masks[1], delta)
-        
-        log_pi_head = self.edge_policy(x, node_id, masks[0])
-        key, head_id, log_pF_head = masking.sample_action(key, log_pi_head, masks[0], delta)
+        log_pi_dep = self.dep_policy(x.mean(axis=-2), x, mask[1])  
+        key, dep_id, log_pF_dep = masking.sample_action(key, log_pi_dep, mask[1], delta)
+         
+        log_pi_head = self.head_policy(x, dep_id, mask[0])
+        key, head_id, log_pF_head = masking.sample_action(key, log_pi_head, mask[0], delta)
         
         return key, (dep_id, head_id), (log_pF_dep, log_pF_head)
 
@@ -54,7 +53,7 @@ class DPGFlowNet(hk.Module):
             output_size=self.model_size, init_scale=self.init_scale, name="Z_flow"
         )(pref).mean(-1)
 
-    def edge_policy(self, x, node_id, masks):
+    def head_policy(self, x, node_id, mask):
         deps = jnp.repeat(x[jnp.newaxis, node_id], x.shape[-2], axis=-2)
         
         # TODO: What if we use edge policy at step 0?
@@ -62,11 +61,11 @@ class DPGFlowNet(hk.Module):
             Biaffine(num_tags=1, init_scale=self.init_scale), in_axes=(0, 0)
         )(x, deps)
         logits = logits.squeeze(-1)
-        log_pi = log_policy(logits, masks)
+        log_pi = log_policy(logits, mask)
 
         return log_pi
 
-    def next_node_policy(self, graph_emb, node_embeddings, masks):
+    def dep_policy(self, graph_emb, node_embeddings, mask):
         graph_embs = jnp.repeat(
             graph_emb[jnp.newaxis, :], node_embeddings.shape[0], axis=0
         )
@@ -78,7 +77,7 @@ class DPGFlowNet(hk.Module):
         )(graph_embs, hidden)
         
         logits = logits.squeeze(-1)
-        log_pi = log_policy(logits, masks)
+        log_pi = log_policy(logits, mask)
 
         return log_pi
     
@@ -86,30 +85,34 @@ class DPGFlowNet(hk.Module):
         pass
 
 
-def log_policy(logits, masks):
-    masks = masks.reshape(logits.shape)
-    logits = masking.mask_logits(logits, masks)
+def log_policy(logits, mask):
+    mask = mask.reshape(logits.shape)
+    logits = masking.mask_logits(logits, mask)
     log_pi = jax.nn.log_softmax(logits, axis=-1)
 
     return log_pi
 
 
 def gflownet_forward_fn(
-    x, node_id, labels, masks, num_tags, num_layers, num_heads, key_size, model_size=None
+    key, x, labels, mask, num_tags, num_layers, num_heads, key_size, delta=0.
 ):
-    # model_size = x.shape[-1]
     num_variables = int(x.shape[0])
 
-    log_pi, next_node_logits = DPGFlowNet(
+    key, actions, (log_pF_dep, log_pF_head) = DPGFlowNet(
         num_variables=num_variables,
         num_tags=num_tags,
         num_layers=num_layers,
         num_heads=num_heads,
         key_size=key_size,
-        model_size=model_size,
-    )(x, node_id, labels, masks)
+    )(key, x, labels, mask, delta)
 
-    return log_pi, next_node_logits
+    return key, actions, (log_pF_dep, log_pF_head)
+
+
+def gflownet_backward_fn(
+    
+):
+    pass
 
 
 def output_total_flow_fn(pref):
