59ea36b6861b503f3b78afa2efe0e872631fad40
nv259 <nguyenvietnhat.03@gmail.com>
2024-11-03 16:23:28 +0700

    superficial changes in utils for bert

--- dp_gfn/utils/pretrains.py
+++ dp_gfn/utils/pretrains.py
@@ -1,4 +1,3 @@
-import jax.numpy as jnp
 import torch
 
 
@@ -29,8 +28,8 @@ def mean_token_embeddings(token_embeddings, word_ids, max_word_length):
     return word_embeddings
 
 
-def get_word_ids_from_tokens(tokens, mask_value=-100):
-    word_ids_list = [tokens.word_ids(i) for i in range(2)]
+def word_ids_from_tokens(tokens, mask_value=-100):
+    word_ids_list = [tokens.word_ids(i) for i in range(len(tokens["input_ids"]))]
     word_ids = torch.tensor(
         [
             [mask_value if val is None else val for val in word_ids]
@@ -41,42 +40,20 @@ def get_word_ids_from_tokens(tokens, mask_value=-100):
     return word_ids
 
 
-def token_embeddings_to_word_embeddings(
+def token_to_word_embeddings(
     token_embeddings: torch.Tensor, 
     word_ids: torch.Tensor, 
     agg_func="mean", 
     max_word_length=160
 ):
     if agg_func == "first":
-        pass
+        raise NotImplementedError
     elif agg_func == "last":
-        pass
+        raise NotImplementedError
     elif agg_func == "mean":
         word_embeddings = mean_token_embeddings(token_embeddings, word_ids, max_word_length)
         
     return word_embeddings
-
-
-def batch_token_embeddings_to_batch_word_embeddings(
-    tokens, token_embeddings, agg_func="mean", max_word_length=160
-):
-    batch_size = token_embeddings.shape[0]
-    batch_word_embeddings = []
-
-    for batch_idx in range(batch_size):
-        batch_word_embeddings.append(
-            token_embeddings_to_word_embeddings(
-                tokens=tokens,
-                token_embeddings=token_embeddings,
-                batch_idx=batch_idx,
-                agg_func=agg_func,
-                max_word_length=max_word_length,
-            )
-        )
-
-    batch_word_embeddings = jnp.stack(batch_word_embeddings)
-
-    return batch_word_embeddings
 
 
 def get_pretrained_parameters(pretrained_weights, keyword="."):
@@ -89,37 +66,15 @@ def get_pretrained_parameters(pretrained_weights, keyword="."):
     return res
 
 
-def split_into_heads(x, num_heads):
-    return jnp.reshape(
-        x,
-        [
-            x.shape[0],  # batch_size
-            x.shape[1],  # seq_len
-            num_heads,
-            x.shape[2] // num_heads,
-        ],
-    )
-
-
 def create_position_ids_from_input_ids(input_ids, padding_idx=1):
-    """
-    Replace non-padding symbols with their position numbers. Position numbers begin at padding_idx+1. Padding symbols
-    are ignored. This is modified from fairseq's `utils.make_positions`.
-
-    Args:
-        input_ids: jnp.ndarray
-        padding_idx: int
-
-    Returns: jnp.ndarray
-    """
     # The series of casts and type-conversions here are carefully balanced to both work with ONNX export and XLA.
     mask = (input_ids != padding_idx).astype("i4")
 
     if mask.ndim > 2:
         mask = mask.reshape((-1, mask.shape[-1]))
-        incremental_indices = jnp.cumsum(mask, axis=1).astype("i4") * mask
+        incremental_indices = torch.cumsum(mask, axis=1).astype("i4") * mask
         incremental_indices = incremental_indices.reshape(input_ids.shape)
     else:
-        incremental_indices = jnp.cumsum(mask, axis=1).astype("i4") * mask
+        incremental_indices = torch.cumsum(mask, axis=1).astype("i4") * mask
 
     return incremental_indices.astype("i4") + padding_idx
