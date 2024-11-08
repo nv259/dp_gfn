9c97ee6cc69389b1c4bf820c33883a9f9107f86d
nv259 <nguyenvietnhat.03@gmail.com>
2024-11-08 17:57:26 +0700

    implement training loop

--- dp_gfn/gfn.py
+++ dp_gfn/gfn.py
@@ -2,16 +2,16 @@ import logging
 import os
 import pickle
 import subprocess
-from collections import namedtuple
 
+import dgl
 import matplotlib.pyplot as plt
 import numpy as np
-from tqdm import trange
-
 import torch
 from torch.utils.data import DataLoader
+from tqdm import trange
 
 from dp_gfn.nets.gflownet import DPGFlowNet
+from dp_gfn.utils import masking, scores
 
 try:
     from evaluation import save_predictions
@@ -28,31 +28,34 @@ class DPGFN:
         self.debug = debug
 
         self.initialize_vars(config)
-        self.model = DPGFlowNet(config.model) 
+        self.model = DPGFlowNet(config.model)
         self.initialize_policy(config.algorithm)
 
         if pretrained_path is not None:
             self.load_weights(pretrained_path)
 
     def initialize_vars(self, config):
+        self.batch_size = config.batch_size
         self.dump_foldername = config.dump_foldername
         self.max_number_of_words = config.max_number_of_words
-        
+
+        nodes_list = [i for i in range(self.max_number_of_words + 1)]
+        self.graph = dgl.batch(
+            [dgl.graph((nodes_list, nodes_list)) for _ in range(self.batch_size)]
+        )
+
         config = config.algorithm
         self.max_steps = config.train.max_steps
         self.eval_every_n = config.train.eval_every_n
         self.save_every_n = config.train.save_every_n
         self.reward_scale_factor = config.reward_scale_factor
-   
-        
-    def sample(
-        self,
-    ):
+
+    def sample(self, states):
         pass
 
     def train(self, train_loader: DataLoader, val_loader: DataLoader):
         save_folder = os.path.join(
-            "/".join(logging.getLogger().handlers[1].baseFilename.split("/")[:-1]),
+            os.path.dirname(logging.getLogger().handlers[1].baseFilename),
             self.dump_foldername,
         )
         os.makedirs(save_folder, exist_ok=True)
@@ -62,26 +65,50 @@ class DPGFN:
             os.makedirs(os.path.join(save_folder, "debug"))
         logging.info(f"Save folder: {save_folder}")
 
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model.to(device)
+
         train_loader = cycle(train_loader)
         train_losses, val_losses = [], []
         train_loss, val_loss = 0, 0
         log_Zs = []
         rewards = []
-            
+
         with trange(self.max_step, desc="Training") as pbar:
             for iteration in pbar:
                 batch = next(train_loader)
-                
-                # sample
-                
-                # calculate loss
-                
-                # update parameter
-                
-                pbar.set_postfix(
-                    
-                ) 
-            
+
+                # Initialize s0 and its tracer
+                state = masking.StateBatch(
+                    self.batch_size,
+                    self.max_number_of_words + 1,
+                    batch["num_words"][0].item(),
+                )
+                word_embeddings = self.model.init_state(
+                    input_ids=batch["input_ids"].to(device),
+                    attention_mask=batch["attention_mask"].to(device),
+                    word_ids=batch["word_ids"].to(device),
+                )
+                self.graph.ndata["s0"] = word_embeddings.repeat(
+                    self.batch_size, 1, 1
+                ).reshape(-1, self.model.bert_model.config.hidden_size)
+
+                log_Z = self.model.logZ(batch["num_words"])
+                complete_states, traj_log_pF, traj_log_pB = self.sample(state)
+                log_R = torch.log(
+                    scores.reward(
+                        complete_states, batch["graph"], scores.frobenius_norm_distance
+                    )
+                    + 1e-9  # offset to prevent -inf from occurring
+                )
+
+                loss = trajectory_balance_loss(log_Z, traj_log_pF, log_R, traj_log_pB)
+
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+
+                pbar.set_postfix()
 
         return train_losses, val_losses
 
@@ -99,7 +126,7 @@ class DPGFN:
         pass
 
 
-def trajectory_balance_loss(log_Z, traj_log_pF, log_R, traj_log_pB, delta=1):
+def trajectory_balance_loss(log_Z, traj_log_pF, log_R, traj_log_pB, delta=1.0):
     assert log_Z.shape == traj_log_pF.shape == traj_log_pB.shape == log_R.shape
     loss = (log_Z + traj_log_pF - log_R - traj_log_pB) ** 2
     logs = {"loss": loss, "log_R": log_R, "log_Z": log_Z}
