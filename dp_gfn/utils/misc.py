5e2f9bcb4c12ac8bd9e111d92fa53f8e0436b107
nv259 <nguyenvietnhat.03@gmail.com>
2024-11-11 15:52:01 +0700

    add util for excluding elements at specified indices

--- dp_gfn/utils/misc.py
+++ dp_gfn/utils/misc.py
@@ -1,3 +1,4 @@
+import torch
 from collections.abc import MutableMapping
 
 
@@ -10,3 +11,22 @@ def flatten_config(d, parent_key="", sep="_"):
         else:
             items.append((new_key, v))
     return dict(items)
+
+
+def remove_at_indices(x, indices):
+    """Creates new embeddings by excluding elements at specified indices.
+
+    Args:
+        x: A tensor of shape (B, N, D) representing the embeddings.
+        indices: A tensor of shape (B, 1) representing the indices to exclude.
+
+    Returns:
+        A tensor of shape (B, N-1, D) containing the new embeddings.
+    """
+
+    B, N, D = x.shape
+
+    all_indices = torch.arange(N).unsqueeze(0).expand(B, -1).to(indices.device)
+    mask = all_indices != indices
+
+    return x[mask].reshape(B, -1, D)
