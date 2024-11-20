d3a06f173f4bc7629987125379bc097333d4c784
nv259 <nguyenvietnhat.03@gmail.com>
2024-11-20 16:22:34 +0700

    minor changes for removing padding nodes when sampling trajectories

--- dp_gfn/gfn.py
+++ dp_gfn/gfn.py
@@ -39,14 +39,7 @@ class DPGFN:
         self.batch_size = config.batch_size
         self.dump_foldername = config.dump_foldername
         self.max_number_of_words = config.max_number_of_words
-
-        nodes_list = [i for i in range(self.max_number_of_words + 1)]
-        self.batched_graph = dgl.batch(
-            [dgl.graph((nodes_list, nodes_list)) for _ in range(self.batch_size)]
-        )
-        
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.batched_graph = self.batched_graph.to(self.device)
 
         config = config.algorithm
         self.max_steps = config.train.max_steps
@@ -67,25 +60,35 @@ class DPGFN:
             {'params': gfn_params, 'lr': config.optimizer.gfn_lr},
         ])
 
-    def sample(self, states, exp_temp=1.0, rand_coef=0.0):
-        traj_log_pF = torch.zeros((self.batch_size,), dtype=torch.float32)
-        traj_log_pB = torch.zeros((self.batch_size,), dtype=torch.float32)
-
+    def sample(self, node_embeddings, states, exp_temp=1.0, rand_coef=0.0):
+        traj_log_pF = torch.zeros((self.batch_size,), dtype=torch.float32, device=self.device)
+        traj_log_pB = torch.zeros((self.batch_size,), dtype=torch.float32, device=self.device)
+        
         actions, log_pF, backward_logits = self.model(
-            self.batched_graph, torch.tensor(states["mask"]).to(self.device), exp_temp, rand_coef
+            node_embeddings=node_embeddings, 
+            graph_relations=torch.tensor(states["relations"], device=self.device), 
+            mask=torch.tensor(states["mask"]).to(self.device), 
+            exp_temp=exp_temp, 
+            rand_coef=rand_coef
         )
 
         for step in range(states.num_words):
             traj_log_pF += log_pF[1] + log_pF[0]
 
-            states.step(actions)
-            prev_actions = actions.copy()
+            np_actions = actions.cpu().numpy()
+            states.step(np_actions)
+            prev_actions = actions.clone()
 
             actions, log_pF, backward_logits = self.model(
-                self.batched_graph, torch.tensor(states["mask"]).to(self.device), exp_temp, rand_coef
+                node_embeddings=node_embeddings, 
+                graph_relations=torch.tensor(states["relations"], device=self.device), 
+                mask=torch.tensor(states["mask"]).to(self.device), 
+                exp_temp=exp_temp, 
+                rand_coef=rand_coef
             )
+            
             traj_log_pB += (
-                backward_logits.log_softmax(1).gather(1, prev_actions[1]).squeeze(-1)
+                backward_logits.log_softmax(1).gather(1, prev_actions[:, 1].unsqueeze(-1)).squeeze(-1)
             )
 
         return states["adjacency"], traj_log_pF, traj_log_pB
@@ -115,7 +118,7 @@ class DPGFN:
                 # Initialize s0 and its tracer
                 state = masking.StateBatch(
                     self.batch_size,
-                    self.max_number_of_words + 1,
+                    batch["num_words"][0].item() + 1, # TODO: Generalize to num_variables
                     batch["num_words"][0].item(),
                 )
                 word_embeddings = self.model.init_state(
@@ -123,20 +126,20 @@ class DPGFN:
                     attention_mask=batch["attention_mask"].to(self.device),
                     word_ids=batch["word_ids"].to(self.device),
                 )
-                self.batched_graph.ndata["s0"] = word_embeddings.repeat(
-                    self.batch_size, 1, 1
-                ).reshape(-1, self.model.bert_model.config.hidden_size)
 
                 log_Z = self.model.logZ(batch["num_words"].to(torch.float32).to(self.device))
-                complete_states, traj_log_pF, traj_log_pB = self.sample(state)
+                complete_states, traj_log_pF, traj_log_pB = self.sample(word_embeddings, state)
                 log_R = torch.log(
                     scores.reward(
-                        complete_states, batch["graph"], scores.frobenius_norm_distance
+                        torch.tensor(complete_states, dtype=torch.float32, device=self.device), 
+                        batch["graph"].to(torch.bool).to(torch.float32).to(self.device), 
+                        scores.frobenius_norm_distance
                     )
                     + 1e-9  # offset to prevent -inf from occurring
                 )
 
-                loss = trajectory_balance_loss(log_Z, traj_log_pF, log_R, traj_log_pB)
+                loss, logs = trajectory_balance_loss(log_Z, traj_log_pF, log_R, traj_log_pB)
+                loss = loss.mean()
 
                 self.optimizer.zero_grad()
                 loss.backward()
@@ -161,11 +164,10 @@ class DPGFN:
 
 
 def trajectory_balance_loss(log_Z, traj_log_pF, log_R, traj_log_pB, delta=1.0):
-    assert log_Z.shape == traj_log_pF.shape == traj_log_pB.shape == log_R.shape
     loss = (log_Z + traj_log_pF - log_R - traj_log_pB) ** 2
-    logs = {"loss": loss, "log_R": log_R, "log_Z": log_Z}
+    logs = {"loss": loss.tolist(), "log_R": log_R.tolist(), "log_Z": log_Z.item()}
 
-    return (loss, logs)
+    return loss, logs
 
 
 def cycle(dataloader):
