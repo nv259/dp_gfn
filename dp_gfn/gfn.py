9e84222b33d4c72fd5b7ae365e14ecaaa76648c6
nv259 <nguyenvietnhat.03@gmail.com>
2024-12-08 23:00:50 +0700

    synthesize trajectories from backward policy (crude version)

--- dp_gfn/gfn.py
+++ dp_gfn/gfn.py
@@ -1,15 +1,15 @@
 import logging
 import os
+import traceback
 
-import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from torch.utils.data import DataLoader
 from tqdm import trange
 
 from dp_gfn.nets.gflownet import DPGFlowNet
-from dp_gfn.utils import masking, scores
-from dp_gfn.utils.replay_buffer import ReplayBuffer
+from dp_gfn.utils import masking, scores, io
+# from dp_gfn.utils.replay_buffer import ReplayBuffer
 
 try:
     from evaluation import save_predictions
@@ -43,35 +43,44 @@ class DPGFN:
         self.max_steps = config.train.max_steps
         self.eval_every_n = config.train.eval_every_n
         self.save_every_n = config.train.save_every_n
-        self.buffer_capacity = config.train.buffer_capacity
+        self.syn_batch_size = config.train.syn_batch_size
+        # self.buffer_capacity = config.train.buffer_capacity
         self.exp_temp = config.train.exp_temp
         self.rand_coef = config.train.rand_coef
         self.p_init = config.train.p_init
         self.reward_scale_factor = config.reward_scale_factor
-        
+
     def initialize_policy(self, config):
         config = config.train
-        
-        bert_params = [params for name, params in self.model.named_parameters() if 'bert' in name]
-        logZ_params = [params for name, params in self.model.named_parameters() if 'logZ' in name]
-        gfn_params = [params for name, params in self.model.named_parameters() if ('bert' not in name) and ('logZ' not in name)]
-        
-        self.optimizer = torch.optim.Adam([
-            {'params': bert_params, 'lr': config.optimizer.gfn_lr * config.optimizer.bert_factor},
-            {'params': logZ_params, 'lr': config.optimizer.Z_lr},
-            {'params': gfn_params, 'lr': config.optimizer.gfn_lr},
-        ])
+
+        bert_params = [params for name, params in self.model.named_parameters() 
+                       if "bert" in name]
+        logZ_params = [params for name, params in self.model.named_parameters() 
+                       if "logZ" in name]
+        gfn_params = [params for name, params in self.model.named_parameters() 
+                      if ("bert" not in name) and ("logZ" not in name)]
+
+        self.optimizer = torch.optim.Adam(
+            [
+                {
+                    "params": bert_params,
+                    "lr": config.optimizer.gfn_lr * config.optimizer.bert_factor,
+                },
+                {"params": logZ_params, "lr": config.optimizer.Z_lr},
+                {"params": gfn_params, "lr": config.optimizer.gfn_lr},
+            ]
+        )
 
     def sample(self, node_embeddings, states, exp_temp=1.0, rand_coef=0.0):
         traj_log_pF = torch.zeros((self.batch_size,), dtype=torch.float32, device=self.device)
         traj_log_pB = torch.zeros((self.batch_size,), dtype=torch.float32, device=self.device)
-        
+
         actions, log_pF, backward_logits = self.model(
-            node_embeddings=node_embeddings, 
-            graph_relations=torch.tensor(states["relations"], device=self.device), 
-            mask=torch.tensor(states["mask"]).to(self.device), 
-            exp_temp=exp_temp, 
-            rand_coef=rand_coef
+            node_embeddings=node_embeddings,
+            graph_relations=torch.tensor(states["relations"], device=self.device),
+            mask=torch.tensor(states["mask"]).to(self.device),
+            exp_temp=exp_temp,
+            rand_coef=rand_coef,
         )
 
         for step in range(states.num_words):
@@ -83,18 +92,63 @@ class DPGFN:
             prev_actions = actions.clone()
 
             actions, log_pF, backward_logits = self.model(
-                node_embeddings=node_embeddings, 
-                graph_relations=torch.tensor(states["relations"], device=self.device), 
-                mask=torch.tensor(states["mask"]).to(self.device), 
-                exp_temp=exp_temp, 
-                rand_coef=rand_coef
+                node_embeddings=node_embeddings,
+                graph_relations=torch.tensor(states["relations"], device=self.device),
+                mask=torch.tensor(states["mask"]).to(self.device),
+                exp_temp=exp_temp,
+                rand_coef=rand_coef,
             )
-            
+
             traj_log_pB += (
-                backward_logits.log_softmax(1).gather(1, prev_actions[:, 1].unsqueeze(-1)).squeeze(-1)
+                backward_logits.log_softmax(1)
+                .gather(1, prev_actions[:, 1].unsqueeze(-1))
+                .squeeze(-1)
             )
 
         return states["adjacency"], traj_log_pF, traj_log_pB
+
+    def synthesize_trajectory(self, node_embeddings, states, graph_relations, exp_temp=1.0, rand_coef=0.0):
+        traj_log_pF = torch.zeros((self.syn_batch_size,), dtype=torch.float32, device=self.device)
+        traj_log_pB = torch.zeros((self.syn_batch_size,), dtype=torch.float32, device=self.device)
+       
+        action_list = self.model.trace_backward(
+            node_embeddings=node_embeddings,
+            graph_relations=graph_relations,
+            exp_temp=exp_temp,
+            rand_coef=rand_coef
+        )
+        
+        log_pF, backward_logits = self.model(
+            node_embeddings=node_embeddings,
+            graph_relations=graph_relations,
+            mask=torch.tensor(states['mask'].to(self.device)),
+            actions=action_list[0],
+            exp_temp=exp_temp,
+            rand_coef=rand_coef
+        ) 
+        
+        for step in range(states.num_words):
+            traj_log_pF += log_pF[1] + log_pF[0]
+
+            np_actions = actions.cpu().numpy()
+            states.step(np_actions)
+
+            actions, log_pF, backward_logits = self.model(
+                node_embeddings=node_embeddings,
+                graph_relations=torch.tensor(states["relations"], device=self.device),
+                mask=torch.tensor(states["mask"]).to(self.device),
+                actions=action_list[step + 1],
+                exp_temp=exp_temp,
+                rand_coef=rand_coef,
+            )
+
+            traj_log_pB += (
+                backward_logits.log_softmax(1)
+                .gather(1, action_list[step][:, 1].unsqueeze(-1))
+                .squeeze(-1)
+            )
+            
+        return traj_log_pF, traj_log_pB
 
     def train(self, train_loader: DataLoader, val_loader: DataLoader):
         save_folder = os.path.join(
@@ -107,7 +161,7 @@ class DPGFN:
         if self.debug:
             os.makedirs(os.path.join(save_folder, "debug"))
         logging.info(f"Save folder: {save_folder}")
-        
+
         # replay = ReplayBuffer(self.buffer_capacity, self.max_number_of_words + 1, self.p_init)
         train_loader = cycle(train_loader)
         train_losses, val_losses = [], []
@@ -118,26 +172,47 @@ class DPGFN:
             for iteration in pbar:
                 batch = next(train_loader)
 
-                # Initialize s0 and its tracer
-                state = masking.StateBatch(
-                    self.batch_size,
-                    batch["num_words"][0].item() + 1, # TODO: Generalize to num_variables
-                    batch["num_words"][0].item(),
-                )
                 word_embeddings = self.model.init_state(
                     input_ids=batch["input_ids"].to(self.device),
                     attention_mask=batch["attention_mask"].to(self.device),
                     word_ids=batch["word_ids"].to(self.device),
                 )
-
-                log_Z = self.model.logZ(batch["num_words"].to(torch.float32).to(self.device))
+                log_Z = self.model.logZ(
+                    batch["num_words"].to(torch.float32).to(self.device)
+                )
                 # log_Z = torch.tensor([4.6], device=self.device)
-                complete_states, traj_log_pF, traj_log_pB = self.sample(word_embeddings, state, self.exp_temp, self.rand_coef)
+                
+                state = masking.StateBatch(
+                    self.batch_size,
+                    batch["num_words"][0].item()
+                    + 1,  # TODO: Generalize to num_variables
+                    batch["num_words"][0].item(),
+                )
+                
+                # Collect trajectories using pF
+                complete_states, traj_log_pF, traj_log_pB = self.sample(
+                    word_embeddings, state, self.exp_temp, self.rand_coef
+                )
+
+                # Synthesize trajectories using pB
+                state.reset(self.syn_batch_size)
+                syn_traj_log_pF, syn_traj_log_pB = self.synthesize_trajectory(
+                    word_embeddings, state, batch["graph"], self.exp_temp, self.rand_coef
+                )
+                
+                # Gather both forward trajectories and backward trajectories
+                complete_states = torch.cat([complete_states, batch["graph"].repeat(self.syn_batch_size, 1, 1)], dim=0)
+                traj_log_pB = torch.cat([traj_log_pB, syn_traj_log_pB], dim=0)
+                traj_log_pF = torch.cat([traj_log_pF, syn_traj_log_pF], dim=0)
+
+                # Calculate reward                
                 log_R = torch.log(
                     scores.reward(
-                        torch.tensor(complete_states, dtype=torch.float32, device=self.device), 
-                        batch["graph"].to(torch.bool).to(torch.float32).to(self.device), 
-                        scores.frobenius_norm_distance
+                        torch.tensor(
+                            complete_states, dtype=torch.float32, device=self.device
+                        ),
+                        batch["graph"].to(torch.bool).to(torch.float32).to(self.device),
+                        scores.frobenius_norm_distance,
                     )
                     + 1e-9  # offset to prevent -inf from occurring
                 )
@@ -148,35 +223,26 @@ class DPGFN:
                 loss.backward()
                 self.optimizer.step()
 
-                rewards.append(logs['log_R'])
-                train_losses.append(logs['loss'])
-                log_Zs.append(logs['log_Z'])
+                rewards.append(logs["log_R"])
+                train_losses.append(logs["loss"])
+                log_Zs.append(logs["log_Z"])
                 pbar.set_postfix(
                     loss=f"{train_losses[-1]:.5f}",
                     reward=f"{np.exp(logs['log_R']).mean():.6f}",
                     Z=f"{np.exp(logs['log_Z']):.5f}",
                 )
-                
+
                 if iteration % self.eval_every_n == 0:
-                    np.save(os.path.join(save_folder, f'rewards_{iteration}.npy'), np.array(rewards))
-                    np.save(os.path.join(save_folder, f'logZ_{iteration}.npy'), np.array(log_Zs))
-                    np.save(os.path.join(save_folder, f'train_losses_{iteration}.npy'), np.array(train_losses))
-                    plt.plot(np.array(train_losses))
-                    plt.savefig(os.path.join(save_folder, f'train_losses_{iteration}.png'))
-                    plt.close()
-                    rewards = []
-                    log_Zs = [] 
-                    train_losses = []
-        
+                    rewards, log_Zs, train_losses = io.save_train_aux(
+                        save_folder, iteration, rewards, log_Zs, train_losses
+                    )
+                
+                
         np.save(os.path.join(save_folder, "train_losses.npy"), np.array(train_losses))
         np.save(os.path.join(save_folder, "logR.npy"), np.array(rewards))
-        np.save(os.path.join(save_folder, "logZ.npy"), np.array(log_Zs)) 
+        np.save(os.path.join(save_folder, "logZ.npy"), np.array(log_Zs))
         return train_losses, val_losses
 
-    def val_step(self, val_loader, *args, **kwargs):
-        losses = []
-
-        return np.mean(losses)
 
     def inference(
         self,
@@ -188,7 +254,7 @@ class DPGFN:
 
 
 def trajectory_balance_loss(log_Z, traj_log_pF, log_R, traj_log_pB, delta=1.0):
-    error = (log_Z + traj_log_pF - log_R - traj_log_pB)
+    error = log_Z + traj_log_pF - log_R - traj_log_pB
     loss = torch.nn.HuberLoss(delta=delta)(error, torch.zeros_like(error))
     logs = {"loss": loss.tolist(), "log_R": log_R.tolist(), "log_Z": log_Z.item()}
 
